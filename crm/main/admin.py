e72c6cba238c09e40198eabb38a0c07a991b467b
ramil <dazimir@yandex.ru>
2023-02-08 08:54:39 +0300

    08-02-23

--- crm/main/admin.py
+++ crm/main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
-from .models import Employee, IndCustomer, URCustomer
+from .models import Employee, IndCustomer, URCustomer, TaskObject
 
 
 class EmployeeInline(admin.StackedInline):
@@ -19,3 +19,5 @@ admin.site.register(User, UserAdmin)
 
 admin.site.register(IndCustomer)
 admin.site.register(URCustomer)
+
+admin.site.register(TaskObject)
--- crm/main/models.py
+++ crm/main/models.py
@@ -4,12 +4,13 @@ from django.contrib.auth.models import User
 import os
 from django.utils import timezone
 
+
 class Employee(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     photo = models.ImageField(blank=True, upload_to='img/', verbose_name='Изображение')
 
 
-#------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------
 
 
 """ DB Заказчик ФЛ """
@@ -30,6 +31,7 @@ class IndCustomer(models.Model):
     ind_date_registration = models.DateField('Дата регистрации')
     ind_sn_passport = models.CharField('Серия и номер паспорта', max_length=15)
     ind_snils = models.CharField('Номер СНИЛС', max_length=14)
+
     # kodnomer = models.CharField('номер клиента', max_length=14)
 
     def __str__(self):
@@ -57,7 +59,6 @@ class URCustomer(models.Model):
     ul_email = models.CharField('email', max_length=50)
     ul_sozdal = models.CharField('Завел карточку', max_length=50)
 
-
     def __str__(self):
         n = str(self.ul_name)
         return n
@@ -68,3 +69,95 @@ class URCustomer(models.Model):
     class Meta:
         verbose_name = 'Заявитель ЮРЛ'
         verbose_name_plural = 'Заявители ЮРЛ'
+
+
+
+
+
+
+
+
+
+
+# ------------------------------------------------------------------------------------
+
+
+""" DB Заказчик ФЛ """
+
+
+class TaskObject(models.Model):
+    task_connection = models.ForeignKey(IndCustomer, null=True, blank=True, on_delete=models.PROTECT, verbose_name='Заявитель физ.лицо')
+    date_card = models.DateField('Дата создания карточки', null=True, default=timezone.now)
+    task_vid = models.CharField('Вид работы', max_length=50)
+    task_kadnom = models.CharField('Кадастровый номер', max_length=50)
+    task_address = models.CharField('Адрес объекта', max_length=250)
+
+    task_nomdogovor = models.CharField('Номер договора', max_length=200, blank=True)
+    task_datedogovor = models.DateField('Дата договора', blank=True)
+    task_skandogovor = models.CharField('Ссылка на договор', max_length=250, blank=True)
+
+    task_kontakt = models.CharField('ФИО контактного лица', max_length=50)
+    task_kontakt_phone = models.CharField('Адрес фактический', max_length=60)
+    task_kontakt_email = models.CharField('Email контактного лица', max_length=50, blank=True)
+    task_kontakt_info = models.CharField('Пояснения', max_length=250, blank=True)
+
+    task_skan_link1 = models.CharField('Документ 1', max_length=250, blank=True)
+    task_nom_link1 = models.CharField('Номер документ 1', max_length=250, blank=True)
+    task_data_link1 = models.DateField('Дата документа 1', blank=True)
+
+    task_skan_link2 = models.CharField('Документ 2', max_length=250, blank=True)
+    task_nom_link2 = models.CharField('Номер документ 2', max_length=250, blank=True)
+    task_data_link2 = models.DateField('Дата документа 2', blank=True)
+
+    task_skan_link3 = models.CharField('Документ 3', max_length=250, blank=True)
+    task_nom_link3 = models.CharField('Номер документ 3', max_length=250, blank=True)
+    task_data_link3 = models.DateField('Дата документа 3', blank=True)
+
+    task_skan_link4 = models.CharField('Документ 4', max_length=250, blank=True)
+    task_nom_link4 = models.CharField('Номер документ 4', max_length=250, blank=True)
+    task_data_link4 = models.DateField('Дата документа 4', blank=True)
+
+    task_skan_link5 = models.CharField('Документ 5', max_length=250, blank=True)
+    task_nom_link5 = models.CharField('Номер документ 5', max_length=250, blank=True)
+    task_data_link5 = models.DateField('Дата документа 5', blank=True)
+
+    task_skan_link6 = models.CharField('Документ 6', max_length=250, blank=True)
+    task_nom_link6 = models.CharField('Номер документ 6', max_length=250, blank=True)
+    task_data_link6 = models.DateField('Дата документа 6', blank=True)
+
+    task_skan_link11 = models.CharField('Документ 11', max_length=250, blank=True)
+    task_nom_link11 = models.CharField('Номер документ 11', max_length=250, blank=True)
+    task_data_link11 = models.DateField('Дата документа 11', blank=True)
+
+    task_skan_link12 = models.CharField('Документ 12', max_length=250, blank=True)
+    task_nom_link12 = models.CharField('Номер документ 12', max_length=250, blank=True)
+    task_data_link12 = models.DateField('Дата документа 12', blank=True)
+
+    task_skan_link13 = models.CharField('Документ 13', max_length=250, blank=True)
+    task_nom_link13 = models.CharField('Номер документ 13', max_length=250, blank=True)
+    task_data_link13 = models.DateField('Дата документа 13', blank=True)
+
+    task_skan_link14 = models.CharField('Документ 14', max_length=250, blank=True)
+    task_nom_link14 = models.CharField('Номер документ 14', max_length=250, blank=True)
+    task_data_link14 = models.DateField('Дата документа 14', blank=True)
+
+    task_skan_link15 = models.CharField('Документ 15', max_length=250, blank=True)
+    task_nom_link15 = models.CharField('Номер документ 15', max_length=250, blank=True)
+    task_data_link15 = models.DateField('Дата документа 15', blank=True)
+
+    task_skan_link16 = models.CharField('Документ 16', max_length=250, blank=True)
+    task_nom_link16 = models.CharField('Номер документ 16', max_length=250, blank=True)
+    task_data_link16 = models.DateField('Дата документа 16', blank=True)
+
+    # kodnomer = models.CharField('номер клиента', max_length=14)
+
+    def __str__(self):
+        n = str(self.task_kadnom + ' ' + self.task_address)
+        return n
+
+    def get_absolute_url(self):
+        return self.id
+
+    class Meta:
+        verbose_name = 'Объект'
+        verbose_name_plural = 'Объекты'
